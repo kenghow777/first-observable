f8bbb5b35cb3358ab3384ac5068c201bc1cbd69f
kenghow777 <93174619+kenghow777@users.noreply.github.com>
2023-05-01 01:32:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# first-observable
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-tuxnae)
---
+++ index.ts
@@ -0,0 +1,27 @@
+console.clear();
+
+import { Observable } from 'rxjs';
+
+// สร้าง object observer ซึ่งถายใน object นี้จะประกอบไปด้วย callback ทั้งหมด 3 ตัว
+// next จะส่งค่ากลับไปที่ observer ซึ่งจะส่งค่าไปเรื่อย ๆ จนกว่าจะครบจำนวน ที่จะแสดง
+// error จะส่งค่ากลับไปที่ observer ซึ่งจะส่งเพียงครั้งเดียว เมื่อเกิด error
+// complete จะส่งค่ากลับไปที่ observer ซึ่งจะส่งเพียงครั้งเดียว เมื่อค่าส่งครบแล้ว
+
+const observer = {
+  next: (value: any) => console.log('next', value),
+  error: (error: any) => console.log('error', error),
+  complete: () => console.log('complete!'),
+};
+
+const observable = new Observable((subscriber) => {
+  subscriber.next('Hello');
+  subscriber.next('World');
+
+  // เมื่อ complete ถูกเรียกแล้ว จะไม่มีการส่งค่าใด ๆ กลับไป
+  subscriber.complete();
+
+  subscriber.next('Hello');
+  subscriber.next('World');
+});
+
+observable.subscribe(observer);
